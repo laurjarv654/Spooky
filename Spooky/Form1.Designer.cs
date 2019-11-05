830ed6e5bc5a1c64f5485d9c316ea4af9fe54828
Laura Jarvis <laurjarv654@ed.amdsb.ca>
2019-11-05 12:33:02 -0500

    Add project files.

---
+++ Spooky/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Spooky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mainStory = new System.Windows.Forms.Label();
+            this.option2 = new System.Windows.Forms.Label();
+            this.option1 = new System.Windows.Forms.Label();
+            this.option3 = new System.Windows.Forms.Label();
+            this.voiceLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(338, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(438, 220);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mainStory
+            // 
+            this.mainStory.BackColor = System.Drawing.Color.Transparent;
+            this.mainStory.ForeColor = System.Drawing.Color.Red;
+            this.mainStory.Location = new System.Drawing.Point(10, 249);
+            this.mainStory.Name = "mainStory";
+            this.mainStory.Size = new System.Drawing.Size(766, 115);
+            this.mainStory.TabIndex = 2;
+            this.mainStory.Text = "label2";
+            // 
+            // option2
+            // 
+            this.option2.AutoSize = true;
+            this.option2.BackColor = System.Drawing.Color.Transparent;
+            this.option2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.option2.Location = new System.Drawing.Point(270, 397);
+            this.option2.Name = "option2";
+            this.option2.Size = new System.Drawing.Size(46, 17);
+            this.option2.TabIndex = 3;
+            this.option2.Text = "label1";
+            // 
+            // option1
+            // 
+            this.option1.AutoSize = true;
+            this.option1.BackColor = System.Drawing.Color.Transparent;
+            this.option1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.option1.Location = new System.Drawing.Point(24, 397);
+            this.option1.Name = "option1";
+            this.option1.Size = new System.Drawing.Size(46, 17);
+            this.option1.TabIndex = 4;
+            this.option1.Text = "label3";
+            // 
+            // option3
+            // 
+            this.option3.AutoSize = true;
+            this.option3.BackColor = System.Drawing.Color.Transparent;
+            this.option3.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.option3.Location = new System.Drawing.Point(24, 440);
+            this.option3.Name = "option3";
+            this.option3.Size = new System.Drawing.Size(46, 17);
+            this.option3.TabIndex = 5;
+            this.option3.Text = "label1";
+            // 
+            // voiceLabel
+            // 
+            this.voiceLabel.BackColor = System.Drawing.Color.Transparent;
+            this.voiceLabel.ForeColor = System.Drawing.Color.Red;
+            this.voiceLabel.Location = new System.Drawing.Point(10, 12);
+            this.voiceLabel.Name = "voiceLabel";
+            this.voiceLabel.Size = new System.Drawing.Size(306, 220);
+            this.voiceLabel.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(816, 514);
+            this.Controls.Add(this.voiceLabel);
+            this.Controls.Add(this.option3);
+            this.Controls.Add(this.option1);
+            this.Controls.Add(this.option2);
+            this.Controls.Add(this.mainStory);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label mainStory;
+        private System.Windows.Forms.Label option2;
+        private System.Windows.Forms.Label option1;
+        private System.Windows.Forms.Label option3;
+        private System.Windows.Forms.Label voiceLabel;
+    }
+}
+
---
+++ Spooky/Form1.cs
@@ -0,0 +1,374 @@
+﻿/// created by : Max Jarvis
+/// date       : 10/23/19
+/// description: A basic text adventure game engine
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Spooky
+{
+    public partial class Form1 : Form
+    {
+        double scene = 0;
+        bool doll = false;
+        bool drawing = false;
+        bool knife = false;
+
+        Graphics g;
+
+        Random randGen = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+           // SoundPlayer 
+            //g = this.CreateGraphics();
+            //Pen drawPen = new Pen(Color.Red, 10);
+            //g.DrawLine(drawPen, 0, 0, 100, 100);
+
+            //g.Clear(Color.White);
+        //    g.Clear(Color.Black);
+            //scene 0 setup
+            mainStory.Text = "You wake up on the floor of an unfamiliar living room. No lights are on and it seems as though no one has lived here in a long time. There are three closed doors. Which one do you choose?";
+            option1.Text = "Door one. (A)";
+            option2.Text = "Door two. (D)";
+            option3.Text = "Door three. (W)";
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int percent = randGen.Next(1, 101);
+
+
+            //option1 (Red on Flowchart)
+            if (e.KeyCode == Keys.A)
+            {
+                //door 1
+                if (scene == 0) { scene = 1; }
+                //laughter
+                else if (scene == 1) { scene = 2; }
+                //back to beginning
+                else if (scene == 2) { scene = 99; }
+                //investigating in the front hall
+                else if (scene == 3) { scene = 2; }
+                //arriving upstairs
+                else if (scene == 4) { scene = 5; }
+                //master bedroom
+                else if (scene == 5 && percent < 65) { scene = 7; }
+                else if (scene == 5 && percent > 65) { scene = 8; }
+                //door three
+                else if (scene == 6) { scene = 6.1; }
+                else if (scene == 6.1) { scene = 6.2; }
+                else if (scene == 6.2) { scene = 21; }
+                //falling asleep in the master bedroom
+                else if (scene == 7) { scene = 22; }
+                //hearing something from the closet
+                else if (scene == 8) { scene = 6; }
+                //bathroom
+                else if (scene == 9) { scene = 10; }
+                //in the foyer with the key
+                else if (scene == 10) { scene = 10.1; }
+                else if (scene == 10.1) { scene = 10.2; }
+                else if (scene==10.2) { scene = 21; }
+                //entering the kitchen
+                else if (scene == 11) { scene = 12; }
+                //kid's drawing
+                else if (scene == 12) { scene = 13; }
+                //knife
+                else if (scene == 13) { scene = 1; }
+                //children's bodies
+                else if (scene == 14 && knife == true) { scene = 18; }
+                else if (scene == 14 && knife == false) { scene = 19; }
+                //hearing their story
+                else if (scene == 15 && doll == true && drawing == true ) { scene = 16; }
+                else if (scene == 15 && doll == true && drawing == false) { scene = 16.1; }
+                else if (scene == 15 && doll == false && drawing == true) { scene = 16.2; }
+                else if (scene == 15 && doll == false && drawing == false) { scene = 17; }
+                //true ending
+                else if (scene == 16) { scene = 23; }
+                else if (scene == 16.1) { scene = 23; }
+                else if (scene == 16.2) { scene = 23; }
+                //failed ending
+                else if (scene == 17&&knife==true) { scene = 18; }
+                else if (scene == 17 && knife == false) { scene = 19; }
+                //failed ending with knife
+                else if (scene == 18) { scene = 21; }
+                //failed ending without knife
+                else if (scene == 19) { scene = 21; }
+                ////play again
+                else if (scene == 21||scene == 22||scene==23) { scene = 99; }
+                //opening screen
+                else if (scene == 99) { scene = 1; }
+            }
+
+            //option 2 (blue on flowchart)
+            if (e.KeyCode == Keys.D)
+            {
+                //door 2
+                if (scene == 0) { scene = 11; }
+                //going upstairs
+                else if (scene == 1) { scene = 4; }
+                //going upstairs after finding doll
+                else if (scene == 3) { scene = 4; }
+                //upstairs, door 2 (bathroom)
+                else if (scene == 4) { scene = 9; }
+                //bedroom, check the closet
+                else if (scene == 5) { scene = 14; }
+                //from bathroom to bedroom
+                else if (scene == 9) { scene = 5; }
+                // from kitchen to foyer
+                else if (scene == 11) { scene = 1; }
+                else if (scene == 12) { scene = 1; }
+                //listening to the children
+                else if (scene == 14) { scene = 15; }
+                //exiting game
+                else if (scene == 21) { this.Close(); }
+                else if (scene == 22) { this.Close(); }
+                else if (scene == 23) { this.Close(); }
+                //door 2
+                else if (scene == 99) { scene = 11; }
+
+            }
+
+            // option 3 (green)
+            if (e.KeyCode == Keys.W)
+            {
+                //door 3
+                if (scene == 0) { scene = 6; }
+                //look around in foyer
+                else if (scene == 1) { scene = 3; }
+                else if (scene == 99) { scene = 6; }
+            }
+
+            switch (scene)
+            {
+                case 99:
+                    voiceLabel.Text = "";
+                    mainStory.Text = "You wake up on the floor of an unfamiliar living room. No lights are on and it seems as though no one has lived here in a long time. There are three closed doors. Which one do you choose?";
+                    option1.Text = "Door one.";
+                    option2.Text = "Door two.";
+                    option3.Text = "Door three.";
+                    break;
+
+                case 1:
+                    mainStory.Text = "You enter the front foyer of the house. You could try to leave through the front door, go up the stairs or investigate a litte.";
+                    option1.Text = "The door.";
+                    option2.Text = "The stairs.";
+                    option3.Text = "Look around.";
+                    break;
+
+                case 2:
+                    voiceLabel.Text = "Haha you thought you could just leave?";
+                    mainStory.Text = "";
+                    option1.Text = "Yes..?";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 3:
+                    mainStory.Text = "You decide to investigate a litte. You find a raggedy, old doll and decide to carry it with you. Now what?";
+                    option1.Text = "Try the door.";
+                    option2.Text = "Go upstairs.";
+                    option3.Text = "";
+                    doll = true;
+                    break;
+
+                case 4:
+                    mainStory.Text = "You are now upstairs. Everything is super dusty... In the hallway are two doors. Which will you choose?";
+                    option1.Text = "Door 1.";
+                    option2.Text = "Door 2.";
+                    option3.Text = "";
+                    break;
+
+                case 5:
+                    mainStory.Text = "You enter the first door to find the master bedroom. Something comes over you and you start to feel really sleepy. Lay in the bed or try to look around?";
+                    option1.Text = "Lay in the bed.";
+                    option2.Text = "search the closet.";
+                    option3.Text = "";
+                    break;
+
+                case 6:
+                    mainStory.Text = "You hear odd noises coming from door three. You open to find the monster hiding inside!  You feel yourself pass out an it's inky black substance surrounds you. You hear a faint voice.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 6.1:
+                    mainStory.Text = "It's...";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 6.2:
+                    mainStory.Text = "It's...  apologizing?";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+
+                case 7:
+                    mainStory.Text = "You lay down and feel sleep come over you. Right before you're about to lose consciousness, you feel someone pull the covers up onto you.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 8:
+                    mainStory.Text = "You lay down and hear a sound come from the closet. With a feeling of unease you decide to go and check it out.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 9:
+                    mainStory.Text = "You open the door and enter what's left of the bathroom. You found a key on the counter! It must be for the front door. Go try it or check out the other room?";
+                    option1.Text = "Try the key.";
+                    option2.Text = "Check out the second room.";
+                    option3.Text = "";
+                    break;
+
+                case 10:
+                    mainStory.Text = "You're back down in the foyer and unlock the door.";
+                    option1.Text = "Coninue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 10.1:
+                    voiceLabel.Text = "Nice try, but that's not going to work here.";
+                    mainStory.Text = "";
+                    option1.Text = "Coninue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 10.2:
+                    mainStory.Text = "You open the door. All you can see is inky blackness. It seems sentient. It traps you.";
+                    option1.Text = "Coninue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 11:
+                    mainStory.Text = "You enter the kitchen. Would you like to move on to the foyer or have a look around?";
+                    option1.Text = "Look around.";
+                    option2.Text = "Head to the foyer.";
+                    option3.Text = "";
+                    break;
+
+                case 12:
+                    mainStory.Text = "You look inside the fridge. Looking for something to munch on, all you find is some moldy cheese. As you go to close the fridge, you notice an old drawing of a family. It looks like it's been drawn by a kid. Something compelled you to keep it so you slipped it into you pocket. Would you like to look inside some drawers or continue to the foyer?";
+                    option1.Text = "Keep looking around.";
+                    option2.Text = "Move on.";
+                    option3.Text = "";
+                    drawing = true;
+                    break;
+
+                case 13:
+                    mainStory.Text = "You find a butcher knife in one of the drawers. You turn it in your hands. Keeping a hold on to it, just in case, you move on to the foyer.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    knife = true;
+                    break;
+
+                case 14:
+                    mainStory.Text = "You enter and see a pile of bodies. The stench makes your eyes water. You hear faint voices whisper in your ear.";
+                    option1.Text = "Struggle and try to escape.";
+                    option2.Text = "Listen to the voices.";
+                    option3.Text = "";
+                    break;
+
+                case 15:
+                    mainStory.Text = "The voices belong to the fallen children. They whisper in you ear and tell you of their stories and the wretched monster that resides in this house. To defeat it, you must face it with evidence of the past children.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 16:
+                    mainStory.Text = "You pull out both the doll and old drawing and turn around to see a child staring at you. They had ink pouring from their eyes and mouth. Upon noticing the doll and drawing, it began to shriek and back away. Instinctively, you held out the objects. The black ink-like substance starting to disappear. You start to feel dizzy and pass out.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 16.1:
+                    mainStory.Text = "You pull out the doll that you had found and turn around to see a child staring at you. They had ink pouring from their eyes and mouth. Upon noticing the doll, it began to shriek and back away. Instinctively, you held out the object. The black ink-like substance starting to disappear. You start to feel dizzy and pass out.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 16.2:
+                    mainStory.Text = "You pull out the old drawing and turn around to see a child staring at you. They had ink pouring from their eyes and mouth. Upon noticing the drawing, it began to shriek and back away. Instinctively, you held out the object. The black ink-like substance starting to disappear. You start to feel dizzy and pass out.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 17:
+                    mainStory.Text = "You search your pockets and realize that you perhaps should have spent more time searching around.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 18:
+                    mainStory.Text = "The voices fade. You slash at the darkness with the knife you found but it's no good. You become one with the darkness.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 19:
+                    mainStory.Text = "The voices fade and you become one with the darkness.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 20:
+                    mainStory.Text = "It's locked! You turn around to come face to face with a horrible creature. An inky black substance surrounds you and you become one with the darkness.";
+                    option1.Text = "Continue.";
+                    option2.Text = "";
+                    option3.Text = "";
+                    break;
+
+                case 21:
+                    mainStory.Text = "YOU DIED. Play again?";
+                    option1.Text = "Yes.";
+                    option2.Text = "No.";
+                    option3.Text = "";
+                    break;
+
+                case 22:
+                    mainStory.Text = "You wake up in your own bed. Turning on the light, you see a small black stain on your shirt. You escaped, but just barely.  Play again?";
+                    option1.Text = "Yes.";
+                    option2.Text = "No.";
+                    option3.Text = ""; 
+                    break;
+
+                case 23:
+                    mainStory.Text = "You wake up at home and look into the mirror. You see that there's a bit of black ink on your shirt but it started to fade away. It seems as though you successfully defeated the monster and set all of the children's souls free.  Play again?";
+                    option1.Text = "Yes.";
+                    option2.Text = "No.";
+                    option3.Text = ""; 
+                    break;
+
+
+            }
+        }
+    }
+}
